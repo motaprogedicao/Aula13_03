a6164ad3238866da94cebcc5397ed1f18aa8b3f5
motaprogedicao <160680888+motaprogedicao@users.noreply.github.com>
2024-09-11 03:21:51 -0300

    Add files via upload

---
+++ app_tattoo/src/view/Principal.java
@@ -0,0 +1,11 @@
+package view;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		// instancia objeto da classe Formulario utilizando, para 
+		// isso, o conceito de "objeto anônimo"
+		new Formulario();
+	}
+}
